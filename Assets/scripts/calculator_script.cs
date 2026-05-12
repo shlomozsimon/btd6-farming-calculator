0081f32d639563c689f0a292c8c2778905220aba
shlomozsimon <shlomozsimon@gmail.com>
2026-05-12 15:08:14 -0500

    finished adding all MK

--- Assets/scripts/calculator_script.cs
+++ Assets/scripts/calculator_script.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 update log: 5/5/2026 started this project and created and shared the git repo as well as create a basic bit a caculations for banana farm (this part got deleted)
  5/10/2026 - restarted from the begging and was able to add starting values and a system for creating mk also decided to create update log
  5/11/2026 - added more MK/more complex MK as well a boat load of temporary variable that i will need to slowly replace one i define the related field
+ 5/12/2026 - finished adding all the MK
 */
 public class CalculatorScript : MonoBehaviour
 {
@@ -14,6 +15,38 @@ public class CalculatorScript : MonoBehaviour
     // These are placeholder variables so your Monkey Knowledge rules compile
     // until the full tower systems are implemented.
     // ---------------------------------------------------------------------
+
+    // Temporary variable for IMF Loan amount.
+    // "Backroom Deals" increases the IMF loan by $1000.
+    private int loan = 10000;
+
+    // Temporary variable for IMF loan repayment percentage.
+    // "Backroom Deals" reduces the repayment amount to 40%.
+    private float repay = 1.0f;   // 100% repayment by default
+
+    // Temporary variable for Monkey Town income bonus.
+    // "Inland Revenue Streams" increases Monkey Town income by 10%.
+    private float monkeyTownBonus = 0.0f;
+
+    // Temporary variable for XXXL Trap RBE capacity.
+    // "Big Traps" increases trap capacity by 30 RBE.
+    private int trapRBE = 1000;
+
+    // Temporary variable for Hero upgrade discount.
+    // "Scholarships" reduces hero upgrade costs by 10%.
+    private float heroUpgradeDiscount = 0.0f;
+
+    // Temporary variable for Hero XP gain bonus.
+    // "Self Taught Heroes" increases XP gain by 10%.
+    private float heroXPGain = 0.0f;
+
+    // Temporary variable for Hero placement discount.
+    // "Hero Favors" reduces hero purchase cost by 10%.
+    private float heroDiscount = 0.0f;
+
+    // Temporary variable for starting Hero level.
+    // "Empowered Heroes" allows heroes to start at level 3.
+    private int heroLvl = 1;
 
     // Temporary variables for Merchantmen / Trade Empire farming.
     // "Trade Agreements" adds +$20 to each Merchantman.
@@ -26,6 +59,18 @@ public class CalculatorScript : MonoBehaviour
     // Temporary variable for Heart of Oak discount.
     // "Warm Oak" reduces the cost by $100.
     private int heartOfOakDiscount = 650;
+
+    // Temporary variable for Heart of farm subsidy.
+    // "farm subsidy" reduces first farm by $100
+    private int cost = 1250;
+
+    // Temporary variable for valuble bananas
+    // "valuble bananas" increased by 5%.
+    private float valubleBananas = 1.25f;
+
+    // temporary variable for bigger banks
+    // "bigger banks" increase max capasity of banks by $2500
+    private int maxHold = 7000;
 
     public enum TowerType{
         farm,
@@ -52,6 +97,11 @@ public class CalculatorScript : MonoBehaviour
     // Used by "Military Conscription".
     [SerializeField]
     private int militaryTowerCount = 0;
+
+    // Number of farms already placed.
+    // Used by "farm subsidy".
+    [SerializeField]
+    private int farmTowerCount = 0;
 
     // Highest relevant upgrade tier for the tower currently being evaluated.
     // Used by "Come On Everybody!"
@@ -233,7 +283,7 @@ public class CalculatorScript : MonoBehaviour
             },
         });
         //adding the mk that will discount village and farms by 2% and increase sell back by 2%
-         monkeyKnowledgeList.Add(new MonkeyKnowledge
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
         {
             MKName = "flat pack buildings",
 
@@ -246,6 +296,129 @@ public class CalculatorScript : MonoBehaviour
                     sellBack += 0.02f;
                 }
             },
+        });
+        //adding the mk that will discount by an adintiale 2%
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "inside trades",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => discount += 0.02f,
+        });
+        //adding the mk that will discount by an adintiale 2%
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "inside trades",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => discount += 0.02f,
+        });
+        //adding the mk that will valuable banaas by an adintiale 5%
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "inside trades",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => valubleBananas += 0.05f,
+        });
+        //adding the mk that will increase max capasity of banks by $2500
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "bigger banks",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => maxHold += 2500,
+        });
+        //adding the mk that increases imf loan by 1000 and a repay rate of 40%
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "backroom deals",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => {
+                loan += 1000;
+                repay = 0.4f;
+            }
+        });
+        //adding the mk that increases monkey town by 10%
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "inland revenue streams",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => monkeyTownBonus += 0.1f,
+        });
+        //adding the mk that increases trap by 30 RBE
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "big traps",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => trapRBE += 30,
+        });
+        //adding the mk that decrease hero cost by 10%
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "scholarships",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => heroUpgradeDiscount = 0.1f,
+        });
+        //adding the mk that increase hero xp gain by 10%
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "self taught heroes",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => heroXPGain = 0.1f,
+        });
+        //adding the mk that increase hero xp gain by 10%
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "self taught heroes",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => heroXPGain = 0.1f,
+        });
+        //adding the mk that decrease placment cost by 10%
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "hero favors",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => heroDiscount = 0.1f,
+        });
+        //adding the mk that lets heros start at level 3
+        monkeyKnowledgeList.Add(new MonkeyKnowledge
+        {
+            MKName = "empowered heroes",
+
+            isActive = true,
+
+            //creating a blank function attached to the mk so if you call this mk you can excute this code
+            MKRule = () => heroLvl = 3,
         });
     }
 }
